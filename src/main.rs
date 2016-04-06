472027561832a08ac153761819845a5d8e2fc7e6
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-04-06 14:48:05 +0100

    entry: Start DVSP service
    Start the UDP service when the gsn is brought online

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@
  * Author:  Charlie Fyvie-Gauld (cfg@zunautica.org)
  * License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
  */
+#![allow(unused_imports)]
 extern crate spring_dvs;
 
-#[allow(unused_imports)]
+
 use spring_dvs::protocol::*;
 use spring_dvs::model::Netspace;
 
 mod netspace;
+mod service;
+mod protocol;
 
 
 fn main() {
     println!("Spring GSN Root Node");
+    match service::start_dvsp() {
+    	Ok(_) => println!("Service finished OK"),
+    	Err(_) => println!("Service finished with error"),
+    }
 }
