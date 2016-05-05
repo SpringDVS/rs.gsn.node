6b439ec1a864ef0538f47755be5aa7a85ea0413d
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-05 14:45:03 +0100

    entry: Add flag for switching on testing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 #![allow(unused_imports)]
 extern crate spring_dvs;
 
+use std::env;
+
 use spring_dvs::model::Netspace;
 use spring_dvs::protocol::*;
 
@@ -19,12 +21,20 @@ mod requests;
 mod unit_test_env;
 
 fn main() {
+	
+	let mut config = config::Config::new();
+	config.live_test = false;
+
+	for a in env::args() {		
+		match a.as_ref() {
+			"--testing" => { config.live_test = true },
+			_ => { }
+		}
+	}
+
     println!("Spring GSN Root Node\n[Node] {}.{}.uk", node_config::node_springname(), node_config::node_geosub());
     println!("[Node] {}/{}", node_config::node_hostname(), node_config::node_resource());
-    let mut config = config::Config::new();
     
-    
-    config.live_test = true;
     match service::Dvsp::start(&config) {
     	Ok(_) =>{  },
     	Err(_) => println!("[Error]"),
