6d3b1e2ba856393bf13e907615e3bea150066d8e
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-06-13 19:39:20 +0100

    sql: Update schema

--- gsn.sql
+++ gsn.sql
@@ -1,18 +1,25 @@
-BEGIN TRANSACTION;
-CREATE TABLE "geosub_netspace" (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`springname`	TEXT UNIQUE,
-	`hostname`	TEXT,
-	`address`	TEXT,
-	`service`	INTEGER,
-	`status`	INTEGER,
-	`types`	INTEGER
-);
-CREATE TABLE "geosub_meta" (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`settlement`	TEXT,
-	`postcode`	TEXT,
-	`county`	TEXT,
-	`geosub`	TEXT
-);
-COMMIT;
+
+		CREATE TABLE `geosub_netspace` (
+			`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+			`springname`	TEXT UNIQUE,
+			`hostname`	TEXT,
+			`address`	TEXT,
+			`service`	INTEGER,
+			`status`	INTEGER,
+			`types`	INTEGER
+		);
+		
+		CREATE TABLE `geotop_netspace` (
+			`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+			`springname`	TEXT,
+			`hostname`	TEXT,
+			`address`	TEXT,
+			`service`	INTEGER,
+			`priority`	INTEGER,
+			`geosub`	TEXT
+		);
+		CREATE TABLE `geosub_tokens` (
+			`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+			`token`	TEXT
+		);
+
