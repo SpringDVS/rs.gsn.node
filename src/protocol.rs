44a032832371bbf2a17faf38640363423f8c4092
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-06-15 19:04:49 +0100

    protocol: Add update tests and clean up macros

--- src/protocol.rs
+++ src/protocol.rs
@@ -39,8 +39,20 @@ fn response(code: Response) -> Message {
 
 pub struct Protocol;
 
+
+macro_rules! valid_src {
+	($node: ident, $svr: ident) => (
+		match Protocol::source_valid(&$node, $svr) {
+			Ok(_) => { }
+			Err(r) => return response(r)
+		}		
+	)
+}
+	
+
 impl Protocol {
 	
+
 	/// Run the action through the system
 	pub fn process(msg: &Message, svr: Svr) -> Message {
 		
@@ -48,6 +60,7 @@ impl Protocol {
 			CmdType::Register => Protocol::register_action(msg, &svr),
 			CmdType::Unregister => Protocol::unregister_action(msg, &svr),
 			CmdType::Info => Protocol::info_action(msg, &svr),
+			CmdType::Update => Protocol::update_action(msg, &svr),
 			_ => Message::from_bytes(b"104").unwrap()
 		}
 		
@@ -69,8 +82,8 @@ impl Protocol {
 	fn unregister_action(msg: &Message, svr: &Svr) -> Message {
 		let single : &ContentNodeSingle = msg_single!(msg.content);
 		let n = Node::from_node_single(&single.nsingle);
-		let addr = ipaddr_str(svr.sock.ip());
-		match Protocol::source_valid(&n, &addr, svr) {
+		
+		match Protocol::source_valid(&n, svr) {
 			Ok(_) => { }
 			Err(r) => return response(r)
 		}
@@ -83,23 +96,68 @@ impl Protocol {
 	}
 	
 	fn info_action(msg: &Message, svr: &Svr) -> Message {
-		let np : &ContentNodeProperty = msg_info_property!(msg.content);
+
+		match msg_info!(msg.content).info {
+			InfoContent::Node(ref np) => Protocol::info_action_node_property(np, svr),
+			InfoContent::Network => Protocol::info_action_network(svr),
+		}
+	}
+	
+	fn info_action_node_property(np: &ContentNodeProperty, svr: &Svr) -> Message {
 		
 		let node : Node = match svr.nio.gsn_node_by_springname(&np.spring) {
 			Ok(n) => n,
 			_ => return response(Response::NetspaceError)
 		};
-		
-		let mut info = node.to_node_info_property(np.property.clone());
+
+		let info = node.to_node_info_property(np.property.clone());
 		let crni = ContentNodeInfo::new( info );
-		
+
 		response_content(Response::Ok, ResponseContent::NodeInfo(crni) )
-		
 	}
 	
-	fn source_valid(n: &Node, addr: &str, svr: &Svr) -> Result<Success,Response> {
+	fn info_action_network(svr: &Svr) -> Message {
+		
+		let mut v : Vec<NodeQuadFmt> = Vec::new();
+		for n in svr.nio.gsn_nodes() {
+			v.push( match n.to_node_quad() {
+					Some(n) => n,
+					None => continue
+				}
+			)
+		}
+
+		response_content (
+			Response::Ok,
+			ResponseContent::Network( ContentNetwork{ network: v } )
+		)
+	}
+	
+	fn update_action(msg: &Message, svr: &Svr) -> Message {
+		let np : &ContentNodeProperty = msg_update!(msg.content);
+		
+		let mut n : Node = match svr.nio.gsn_node_by_springname(&np.spring) {
+			Ok(n) => n,
+			_ => return response(Response::NetspaceError)
+		};
+		
+		valid_src!(n, svr);
+		
+		let state = match np.property {
+			NodeProperty::State(Some(s)) => s,
+			_ => return response(Response::UnsupportedAction),
+		};
+		
+		n.update_state(state);
+		match svr.nio.gsn_node_update_state(&n) {
+			Ok(Success::Ok) => response(Response::Ok),
+			_ => response(Response::NetspaceError),
+		}		
+	}
+	
+	fn source_valid(n: &Node, svr: &Svr) -> Result<Success,Response> {
 		match svr.nio.gsn_node_by_springname(n.springname()) {
-			Ok(n) =>  match n.address() == addr {
+			Ok(n) =>  match n.address() == ipaddr_str(svr.sock.ip()) {
 				true => Ok(Success::Ok),
 				false => Err(Response::NetworkError),
 			},
@@ -166,6 +224,16 @@ mod tests {
 		)
 	}
 	
+	macro_rules! assert_ok{
+		($msg: expr, $svr: ident) => ( {
+			let m = Protocol::process(&new_msg($msg), $svr);
+			assert_eq!(m.cmd, CmdType::Response);
+			assert_match!(m.content, MessageContent::Response(_));
+			assert_eq!(msg_response!(m.content).code, Response::Ok);
+			m
+		} )
+	}
+	
 	fn new_netspace() -> NetspaceIo {
 		let ns = NetspaceIo::new(":memory:");
 		ns.db().execute("
@@ -208,8 +276,16 @@ mod tests {
 	}
 	
 	fn add_node(ns: &Netspace) {
-		ns.gsn_node_register(&Node::from_str("spring:foo,host:foobar,address:172.168.1.1,role:hub,service:http,state:enabled").unwrap());
-	} 
+		ns.gsn_node_register(&Node::from_str("spring:foo,host:foobar,address:192.168.1.2,role:hub,service:http,state:enabled").unwrap());
+	}
+
+	fn add_remote_node(ns: &Netspace) {
+		ns.gsn_node_register(&Node::from_str("spring:foo,host:foobar,address:192.168.1.3,role:hub,service:http,state:enabled").unwrap());
+	}
+	
+	fn get_node(s: &str, ns: &Netspace) -> Node {
+		try_panic!(ns.gsn_node_by_springname(s))
+	}
 	
 	#[test]
 	fn ts_protocol_register_pass() {
@@ -293,11 +369,8 @@ mod tests {
 		//Add already registered
 		add_node(&ns);
 		
-		let m = Protocol::process(&new_msg("info node foo hostname"), svr);
-		assert_eq!(m.cmd, CmdType::Response);
-		assert_match!(m.content, MessageContent::Response(_));
-		
-		assert_eq!(msg_response!(m.content).code, Response::Ok);
+		let m = assert_ok!("info node foo hostname", svr);
+
 		assert_match!(msg_response!(m.content).content, ResponseContent::NodeInfo(_));
 		let ni = msg_response_nodeinfo!(m.content);
 		
@@ -317,17 +390,14 @@ mod tests {
 		//Add already registered
 		add_node(&ns);
 		
-		let m = Protocol::process(&new_msg("info node foo address"), svr);
-		assert_eq!(m.cmd, CmdType::Response);
-		assert_match!(m.content, MessageContent::Response(_));
-		
-		assert_eq!(msg_response!(m.content).code, Response::Ok);
+		let m = assert_ok!("info node foo address", svr);
+
 		assert_match!(msg_response!(m.content).content, ResponseContent::NodeInfo(_));
 		let ni = msg_response_nodeinfo!(m.content);
 		
 		assert!(ni.info.host.is_empty());
 		assert!(ni.info.spring.is_empty());
-		assert_eq!(ni.info.address, "172.168.1.1");
+		assert_eq!(ni.info.address, "192.168.1.2");
 		assert_eq!(ni.info.service, NodeService::Undefined);
 		assert_eq!(ni.info.state, NodeState::Unspecified);
 		assert_eq!(ni.info.role, NodeRole::Undefined);	
@@ -341,11 +411,8 @@ mod tests {
 		//Add already registered
 		add_node(&ns);
 		
-		let m = Protocol::process(&new_msg("info node foo service"), svr);
-		assert_eq!(m.cmd, CmdType::Response);
-		assert_match!(m.content, MessageContent::Response(_));
-		
-		assert_eq!(msg_response!(m.content).code, Response::Ok);
+		let m = assert_ok!("info node foo service", svr);
+
 		assert_match!(msg_response!(m.content).content, ResponseContent::NodeInfo(_));
 		let ni = msg_response_nodeinfo!(m.content);
 		
@@ -365,11 +432,8 @@ mod tests {
 		//Add already registered
 		add_node(&ns);
 		
-		let m = Protocol::process(&new_msg("info node foo state"), svr);
-		assert_eq!(m.cmd, CmdType::Response);
-		assert_match!(m.content, MessageContent::Response(_));
-		
-		assert_eq!(msg_response!(m.content).code, Response::Ok);
+		let m = assert_ok!("info node foo state", svr);
+
 		assert_match!(msg_response!(m.content).content, ResponseContent::NodeInfo(_));
 		let ni = msg_response_nodeinfo!(m.content);
 		
@@ -389,11 +453,7 @@ mod tests {
 		//Add already registered
 		add_node(&ns);
 		
-		let m = Protocol::process(&new_msg("info node foo role"), svr);
-		assert_eq!(m.cmd, CmdType::Response);
-		assert_match!(m.content, MessageContent::Response(_));
-		
-		assert_eq!(msg_response!(m.content).code, Response::Ok);
+		let m = assert_ok!("info node foo role", svr);
 		assert_match!(msg_response!(m.content).content, ResponseContent::NodeInfo(_));
 		let ni = msg_response_nodeinfo!(m.content);
 		
@@ -413,17 +473,14 @@ mod tests {
 		//Add already registered
 		add_node(&ns);
 		
-		let m = Protocol::process(&new_msg("info node foo all"), svr);
-		assert_eq!(m.cmd, CmdType::Response);
-		assert_match!(m.content, MessageContent::Response(_));
-		
-		assert_eq!(msg_response!(m.content).code, Response::Ok);
+		let m = assert_ok!("info node foo all", svr);
+
 		assert_match!(msg_response!(m.content).content, ResponseContent::NodeInfo(_));
 		let ni = msg_response_nodeinfo!(m.content);
 		
 		assert_eq!(ni.info.host, "foobar");
 		assert_eq!(ni.info.spring, "foo");
-		assert_eq!(ni.info.address, "172.168.1.1");
+		assert_eq!(ni.info.address, "192.168.1.2");
 		assert_eq!(ni.info.service, NodeService::Http);
 		assert_eq!(ni.info.state, NodeState::Disabled);
 		assert_eq!(ni.info.role, NodeRole::Hub);	
@@ -442,5 +499,128 @@ mod tests {
 		
 		assert_eq!(msg_response!(m.content).code, Response::NetspaceError);
 		
+	}
+	
+	#[test]
+	fn ts_protocol_info_network_fail_unsupported_action() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		let m = assert_ok!("info network", svr);
+		
+	}
+	
+	#[test]
+	fn ts_protocol_update_state_unspecified_pass() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		assert_ok!("update foo state unspecified", svr);
+		
+		let n = get_node("foo", &ns);		
+		assert_eq!(n.state(), NodeState::Unspecified);
+			
+	}
+	
+	#[test]
+	fn ts_protocol_update_state_enabled_pass() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		assert_ok!("update foo state enabled", svr);
+		
+		let n = get_node("foo", &ns);
+		assert_eq!(n.state(), NodeState::Enabled);
+			
+	}
+	
+	#[test]
+	fn ts_protocol_update_state_disabled_pass() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		assert_ok!("update foo state disabled", svr);
+		
+		let n = get_node("foo", &ns);
+		assert_eq!(n.state(), NodeState::Disabled);
+			
+	}
+	
+	#[test]
+	fn ts_protocol_update_state_unresponsive_pass() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		assert_ok!("update foo state unresponsive", svr);
+
+		let n = get_node("foo", &ns);
+		assert_eq!(n.state(), NodeState::Unresponsive);
+			
+	}
+
+	#[test]
+	fn ts_protocol_update_state_fail_no_node() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		let m = Protocol::process(&new_msg("update void state unspecified"), svr);
+		assert_eq!(m.cmd, CmdType::Response);
+		assert_match!(m.content, MessageContent::Response(_));
+		
+		assert_eq!(msg_response!(m.content).code, Response::NetspaceError);
+		
+			
+	}
+	
+	#[test]
+	fn ts_protocol_update_state_fail_unsupported_action() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_node(&ns);
+		
+		let m = Protocol::process(&new_msg("update foo role hub"), svr);
+		assert_eq!(m.cmd, CmdType::Response);
+		assert_match!(m.content, MessageContent::Response(_));
+		
+		assert_eq!(msg_response!(m.content).code, Response::UnsupportedAction);
+		
+			
+	}
+	
+	#[test]
+	fn ts_protocol_update_state_fail_network_error() {
+		let ns = new_netspace();
+		let svr = new_svr(&ns);
+
+		//Add already registered
+		add_remote_node(&ns);
+		
+		let m = Protocol::process(&new_msg("update foo state unresponsive"), svr);
+		assert_eq!(m.cmd, CmdType::Response);
+		assert_match!(m.content, MessageContent::Response(_));
+		
+		assert_eq!(msg_response!(m.content).code, Response::NetworkError);
+		
+			
 	}
 }
