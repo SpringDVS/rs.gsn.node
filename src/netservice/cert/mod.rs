5b253110ba15a0562ecaa7a47127f2b9e8501a88
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-10-12 16:32:26 +0100

    netservice: Add handling for cert pulls
    This enables a primary server to provide keys when they are pulled from
    the keyring.
    
    Pull requests made to the server are an unsupported action at this time.

--- src/netservice/cert/mod.rs
+++ src/netservice/cert/mod.rs
@@ -8,12 +8,12 @@ use std::collections::BTreeMap;
 use rustc_serialize::json::{self, ToJson, Json};
 
 
-use ::spring_dvs::protocol::{Message, generate_response_service_text};
+use ::spring_dvs::protocol::{Message, generate_response_service_text, generate_response_empty_code};
 use ::spring_dvs::uri::Uri;
 
 use ::protocol::Svr;
 
-use self::keyring::{Certificate,Key};
+use self::keyring::{Certificate,Key,Keyring};
 
 #[derive(RustcEncodable,Debug,Clone)]
 enum Response {
@@ -50,7 +50,15 @@ pub fn request(uri: &Uri, svr: &Svr) -> Message {
 	match uri.res_index(1) {
 		None => request_certificate(svr),
 		Some("key") => request_key(svr),
-		_ => service_response(Response::Certificate(Certificate::error()), svr)
+		Some("pull") => {
+			let keyid = match uri.res_index(2) {
+				Some(s) => s,
+				None => return service_response(Response::Key(Key::error()), svr)
+			};
+			handle_pull(keyid,svr)
+		},
+		Some("pullreq") => generate_response_empty_code(::spring_dvs::protocol::Response::UnsupportedAction),
+		_ => generate_response_empty_code(::spring_dvs::protocol::Response::MalformedContent)
 	}
 	
 }
@@ -73,6 +81,16 @@ fn request_key(svr: &Svr) -> Message {
 	}
 	
 	service_response(Response::Key(Key::new(&s)), svr)
+}
+
+fn handle_pull(keyid: &str, svr: &Svr) -> Message {
+	let kr = Keyring::new();
+	let cert = match kr.with_keyid(keyid) {
+		None => return service_response(Response::Key(Key::error()), svr),
+		Some(c) => c
+	};
+	
+	service_response(Response::Key(Key::new(cert.armor())), svr)	
 }
 
 fn service_response(response: Response, svr: &Svr) -> Message {
