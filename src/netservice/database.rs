59fa38cd8264be515bc8006aab6c11f93d8c591c
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-10-12 15:57:40 +0100

    netservice: Database exports sqlite uses

--- src/netservice/database.rs
+++ src/netservice/database.rs
@@ -1,10 +1,10 @@
 extern crate sqlite;
-pub use self::sqlite::{State,Statement};
+pub use self::sqlite::{State,Statement,Value,Connection};
 	
 pub struct ServiceDatabase;
 
 impl ServiceDatabase {
-	pub fn new() -> sqlite::Connection {
+	pub fn new() -> Connection {
 		sqlite::open("/var/lib/springdvs/services.db").unwrap()
 	}
 }
